56df92b14c67780b8faea3fbb8f454c3a077b207
Raissa Soares Cesar <raissa.soares.cesar@gmal.com>
2026-01-21 12:12:50 -0300

    mongo up

--- src/main/java/br/com/fiap/postech/soat/techchallenge/infraestructure/persistence/CustomerDocument.java
+++ src/main/java/br/com/fiap/postech/soat/techchallenge/infraestructure/persistence/CustomerDocument.java
@@ -1,10 +1,13 @@
 package br.com.fiap.postech.soat.techchallenge.infraestructure.persistence;
 
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "customers")
@@ -15,8 +18,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class CustomerDocument {
     @Id
     private String id;
+
+    @Indexed(unique = true)
     private String name;
+
+    @CPF
     private String cpf;
+
+    @Email
     private String email;
+
     private String phone;
 }
